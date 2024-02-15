9ea60daeb9c1b9c5cba041973fc88e22ecf45f01
gilbaek <iwbake@gmail.com>
2024-02-15 11:41:51 +0900

    first commit

---
+++ java01.jdbc/src/com/lec/ex01_basic/DeleteMain.java
@@ -0,0 +1,11 @@
+package com.lec.ex01_basic;
+
+public class DeleteMain {
+
+	public static void main(String[] args) {
+		
+//		delete from employees where employee_id = ?
+//		System.out.println(row + "건이 삭제되었습니다!!");
+	}
+
+}
